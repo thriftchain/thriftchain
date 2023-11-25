f4bf025748893c11e1dadbd194b36adcfcdc1925
pheobeayo <pheobeayo@gmail.com>
2023-11-25 19:34:03 +0100

    hero
    hero

--- src/components/thrift/Thrift.js
+++ src/components/thrift/Thrift.js
@@ -1,5 +1,6 @@
 import React from "react";
 import objects from "../../assets/objects.svg";
+import { Link } from "react-router-dom";
 
 
 const Thrift = () => {
@@ -7,36 +8,39 @@ const Thrift = () => {
 
 
 
-    return (
-       
-            <main className='bg-[#070624]'>
-             
-                <h1 className='text-[25px] md:text-[30px] font-bold capitalize  text-[#BD5FB8] mx-80 mt-10 px-8' >ThriftChain: Your Key to Financial Freedom </h1>
-                <h2 className='text-[25px] md:text-[20px] font-bold capitalize  text-white  mx-96 px-8'>Unlock Savings, Achieve Dreams, Secure Tomorrow</h2>
-               <div className='mx-96 px-60 mt-4'> <button class="bg-gradient-to-r from-[#9C0F94] to-[#142698] hover:bg-blue-700 text-white font-semibold py-2 rounded border-white border-spacing-1 ">
-          Start Saving    </button></div>
-         
-          <div className="mx-80 px-16 mt-4">
-            <img src={objects} alt='objects'/>
-            <h1 className='text-[30px] md:text-[30px] font-bold capitalize  text-white mx-32' >About ThriftChain </h1>
-            <p className="text-[14px] md:text-[14px] text-white mb-4">ThriftChain is a blockchain-based financial management platform. It enables individual and 
-                <br></br>group savings, contributions, and goal achievement. Users can track progress in real-time, 
-                <br></br>and blockchain technology ensures security and transparency. ThriftChain encourages 
-                <br></br>financial discipline, making it a powerful tool for both individual and group financial 
-                <br></br>planning.</p>
+  return (
 
-          </div>
-            </main>
+    <main className='bg-[#070624]'>
+      <div className='w-100 md:w-[100%] lg:p-16 p-4  grid lg:grid-cols-2 h-[80vh] lg:gap-0 gap-4'>
+        <div className="flex flex-col gap-4">
+          <h1 className='text-[25px] md:text-[30px] font-bold capitalize  text-[#BD5FB8]' >ThriftChain: Your Key to Financial Freedom </h1>
+          <h2 className='text-[25px] md:text-[20px] font-bold capitalize  text-white'>Unlock Savings, Achieve Dreams, Secure Tomorrow</h2>
+          <div > <Link to='/'><button class="bg-gradient-to-r from-[#9C0F94] to-[#142698] hover:bg-blue-700 text-white font-semibold py-2 rounded border-white border-spacing-1 ">
+            Start Saving    </button> </Link></div>
+          <h1 className='text-[30px] md:text-[30px] font-bold capitalize  text-white' >About ThriftChain </h1>
+          <p className="text-[15px] md:text-[15px] text-white mb-4">ThriftChain is a blockchain-based financial management platform. It enables individual and
+            <br></br>group savings, contributions, and goal achievement. Users can track progress in real-time,
+            <br></br>and blockchain technology ensures security and transparency. ThriftChain encourages
+            <br></br>financial discipline, making it a powerful tool for both individual and group financial
+            <br></br>planning.</p>
 
+        </div>
+        <div >
+          <img src={objects} alt='objects' />
 
-       
-
-
+        </div>
+      </div>
+    </main>
 
 
 
 
-    )
+
+
+
+
+
+  )
 
 
 
--- src/pages/message/Message.js
+++ src/pages/message/Message.js
@@ -9,52 +9,52 @@ import { AiOutlineTeam } from "react-icons/ai";
 const Message = () => {
     return (
         <div className='bg-[#070624] '>
-            <div className="container mx-auto px-4 sm:px-[80px] py-[100px]">
 
-                <div class="grid grid-cols-2 divide-x h-full">
-                    <div class='bg-[#060E37]'>
-                        <h2 className="text-[30px] md:text-[32px] font-bold capitalize text-[#BD5FB8]  mx-4">
-                            DreamSavers
-                        </h2>
-                        <div className='mx-4'><Link to='/group-overview'> <button class="bg-gradient-to-r from-[#9C0F94] to-[#142698] hover:bg-blue-700 text-white font-normal py-2 rounded-full border-white border-spacing-1 ">
-                            Group overview    </button></Link>
-                            <Link to='/'> <button class="bg-[#2C2C2C] hover:bg-blue-700 text-white font-normal py-2 rounded-full border-white border-spacing-1 mx-4">
-                                Exit group    </button></Link>
-                        </div>
-                        <h2 className="text-[20px] md:text-[20px] font-bold capitalize text-[#BD5FB8]  mx-4 mt-6">
-                            Rooms
-                        </h2>
-                        <div
-                            class="mx-3 mt-6 flex flex-col rounded-lg bg-white sm:shrink-0 sm:grow sm:basis-0 border-white border-2 w-1/2 h-1/2">
-                            <img
-                                src={general}
-                                alt="general"
-                                class='object-center w-1/4 rounded-t-lg justify-center mx-24'
-                            />
 
-                            <div className="w-full h-1/2 mx-16">
-                                <h3 className="font-bold text-black text-base">
-                                    General channel
-                                </h3>
-                                <div class='mx-4 grid-flow-row'> <p className="font-normal  text-black text-xs ">
-                                    <AiOutlineTeam />500k
+            <div class='grid grid-cols-1 md:flex md:flex-row gap-2'>
+                <div class='bg-[#060E37]'>
+                    <h2 className="text-[30px] md:text-[32px] font-bold capitalize text-[#BD5FB8]  mx-4">
+                        DreamSavers
+                    </h2>
+                    <div className='mx-4'><Link to='/group-overview'> <button class="bg-gradient-to-r from-[#9C0F94] to-[#142698] hover:bg-blue-700 text-white font-normal py-2 rounded-full border-white border-spacing-1 ">
+                        Group overview    </button></Link>
+                        <Link to='/'> <button class="bg-[#2C2C2C] hover:bg-blue-700 text-white font-normal py-2 rounded-full border-white border-spacing-1 mx-4">
+                            Exit group    </button></Link>
+                    </div>
+                    <h2 className="text-[20px] md:text-[20px] font-bold capitalize text-[#BD5FB8]  mx-4 mt-6">
+                        Rooms
+                    </h2>
+                    <div
+                        class="mx-3 mt-6 flex flex-col rounded-lg bg-white sm:shrink-0 sm:grow sm:basis-0 border-white border-2 w-1/2 h-1/2">
+                        <img
+                            src={general}
+                            alt="general"
+                            class='object-center w-1/4 rounded-t-lg justify-center mx-24'
+                        />
 
-                                </p>
-                                </div>
+                        <div className="w-full h-1/2 mx-16">
+                            <h3 className="font-bold text-black text-base">
+                                General channel
+                            </h3>
+                            <div class='mx-4 grid-flow-row'> <p className="font-normal  text-black text-xs ">
+                                <AiOutlineTeam />500k
+
+                            </p>
                             </div>
                         </div>
                     </div>
+                </div>
 
 
-                    <div class='bg-[#070624]'>
-                        <div class='bg-[#060E37]'>
-                            <h2 className="text-[30px] md:text-[32px] font-bold capitalize text-[#BD5FB8]  mx-4">
-                                Overview <span className="text-white mx-10">Assets.</span>
-                            </h2>
-                        </div>
+                <div class='bg-[#070624]'>
+                    <div class='bg-[#060E37]'>
+                        <h2 className="text-[30px] md:text-[32px] font-bold capitalize text-[#BD5FB8]  mx-4">
+                            Overview <span className="text-white mx-10">Assets.</span>
+                        </h2>
                     </div>
                 </div>
             </div>
+
         </div>
 
 
